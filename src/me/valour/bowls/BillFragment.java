8d072e3ce5c6e7d67c2be5c13110c6cb40a664cb
Alice <alice@valour.me>
2014-03-15 15:37:18 -0400

    new fragment layout

---
+++ src/me/valour/bowls/BillFragment.java
@@ -0,0 +1,17 @@
+package me.valour.bowls;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class BillFragment extends Fragment {
+
+	@Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_bill, container, false);
+    }
+}
